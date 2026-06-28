015afd52ec42420a759c77b26a8afd02cb58f1f0
NuwasasiraBarigyenda <nuwasasirabarigyenda@gmail.com>
2026-06-28 23:32:37 +0300

    Refactor CurrentAccount to extend Account class
    Refactored CurrentAccount class to extend Account and implement required methods.

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,13 +1,25 @@
-package com.firstbank.model;
+// current account - min deposit 200,000 UGX
+// overdraft allowed, no interest earned
 
-public class CurrentAccount {
-    public CurrentAccount(String a, String b, String c, String d,  String e, String f, String g, String h, double i) {
-        super();
+public class CurrentAccount extends Account { // extends Account = inherits all shared fields and summary()
+
+    // passes all details up to the Account constructor
+    public CurrentAccount(String a, String b, String c, String d,
+                          String e, String f, String g, String h, double i) {
+        super(a, b, c, d, e, f, g, h, i); // Account stores everything
     }
-    public int minimumDeposit() {
-        return 200000;}
-        public String accountTypeName(){
-            return "Current";
-        }
+
+    // polymorphism - returns 200,000 for current accounts
+    @Override // replacing the abstract method from Account
+    public double minimumDeposit() {
+        return 200000;
     }
+
+    // returns the account type label used in the summary line
+    @Override // replacing the abstract method from Account
+    public String accountTypeName() {
+        return "Current";
+    }
+}
+
 
